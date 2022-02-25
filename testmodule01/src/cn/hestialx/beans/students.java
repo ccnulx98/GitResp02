3a5215037b14e10d2740a2ce805c8d9b312270cd
lx <2444171739@qq.com>
2022-02-25 20:55:52 +0800

    第一次提交

---
+++ testmodule01/src/cn/hestialx/beans/students.java
@@ -0,0 +1,26 @@
+package cn.hestialx.beans;
+
+/**
+ * @author lixu
+ * @create 2022-02-25-20:45
+ */
+public class students {
+    String name;
+    int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
